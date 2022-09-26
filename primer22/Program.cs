9ae2a4f8e0475717f47067f61e44dfee67dce6ee
mikle <mr.mikle@mail.ru>
2022-09-26 16:23:24 +0300

    primer22

---
+++ primer22/Program.cs
@@ -0,0 +1,15 @@
+﻿// 22 принимает на вход N и выдает таблицу квадратов чисел от 1 до N
+// 5 -> 1,4,9,16,25
+// 2-> 1,4
+
+Console.Clear();
+Console.WriteLine("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+//int[] array1 = new int[N];
+int i = 1;
+while (i <= N)
+{
+    int sqr = i*i;
+    Console.WriteLine(sqr);
+    i++;
+}
